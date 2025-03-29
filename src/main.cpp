317eda8151e1de0ea182cdbb9ac5eebad8ad7812
JamesHarden313 <shield430024053@gmail.com>
2025-03-29 23:27:11 +0800

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <std_msgs/Int32.h>
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "sensor_msgs/LaserScan.h"
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 #include "uart_driver.h"
 using namespace std;
 
@@ -16,199 +16,215 @@ double angle_disable_min = -1;
 double angle_disable_max = -1;
 io_driver driver;
 
-void publish_scan(ros::Publisher *pub, double *dist, double *intensities, int count, ros::Time start, double scan_time)
+void publish_scan(const rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr pub, 
+                 double *dist, double *intensities, int count, 
+                 rclcpp::Time start, double scan_time)
 {
-	static int scan_count = 0;
-	sensor_msgs::LaserScan scan_msg;
-	scan_msg.header.stamp = start;
-	scan_msg.header.frame_id = laser_link;
-	scan_count++;
-	// scan_msg.angle_min = (angle_disable_min < 0) ? 0 : angle_disable_min;
-	// scan_msg.angle_max = (angle_disable_max < 0) ? 2 * M_PI : angle_disable_max;
-	scan_msg.angle_min = 0.0;
-	scan_msg.angle_max = 2 * M_PI;
-	scan_msg.angle_increment = (scan_msg.angle_max - scan_msg.angle_min) / (double) (count - 1);
-	scan_msg.scan_time = scan_time;
-	scan_msg.time_increment = scan_time / (double) (count - 1);
+    static int scan_count = 0;
+    auto scan_msg = std::make_shared<sensor_msgs::msg::LaserScan>();
+    scan_msg->header.stamp = start;
+    scan_msg->header.frame_id = laser_link;
+    scan_count++;
+    scan_msg->angle_min = 0.0;
+    scan_msg->angle_max = 2 * M_PI;
+    scan_msg->angle_increment = (scan_msg->angle_max - scan_msg->angle_min) / (double)(count - 1);
+    scan_msg->scan_time = scan_time;
+    scan_msg->time_increment = scan_time / (double)(count - 1);
 
-	scan_msg.range_min = 0.1;
-	scan_msg.range_max = 10.0;
+    scan_msg->range_min = 0.1;
+    scan_msg->range_max = 10.0;
 
-	scan_msg.intensities.resize(count);
-	scan_msg.ranges.resize(count);
+    scan_msg->intensities.resize(count);
+    scan_msg->ranges.resize(count);
 
-	if (!inverted) {
+    if (!inverted) {
         for (int i = count - 1; i >= 0; i--) {
             if (dist[count - i - 1] == 0.0 && zero_as_max)
-                scan_msg.ranges[i] = scan_msg.range_max - 0.2;
+                scan_msg->ranges[i] = scan_msg->range_max - 0.2;
             else if (dist[count - i - 1] == 0.0)
                 if (min_as_zero)
-                    scan_msg.ranges[i] = 0.0;
+                    scan_msg->ranges[i] = 0.0;
                 else
-                    scan_msg.ranges[i] = std::numeric_limits<float>::infinity();
+                    scan_msg->ranges[i] = std::numeric_limits<float>::infinity();
             else
-                scan_msg.ranges[i] = dist[count - i - 1] / 1000.0;
-            scan_msg.intensities[i] = floor(intensities[count - i - 1]);
+                scan_msg->ranges[i] = dist[count - i - 1] / 1000.0;
+            scan_msg->intensities[i] = floor(intensities[count - i - 1]);
         }
     }
-	else
-	{
-		for (int i = 0; i <= 179; i++)
-			{
-				if(dist[179-i] == 0.0 && zero_as_max)
-					scan_msg.ranges[i] = scan_msg.range_max - 0.2;
-				else if(dist[179-i] == 0.0)
-					if (min_as_zero)
-						scan_msg.ranges[i] = 0.0;
-					else
-						scan_msg.ranges[i] = std::numeric_limits<float>::infinity();
-				else
-					scan_msg.ranges[i] = dist[179-i] / 1000.0;
-				scan_msg.intensities[i] = floor(intensities[179-i]);
-			}
-		for (int i = 180; i < 360; i++)
-			{
-				if(dist[540-i] == 0.0 && zero_as_max)
-					scan_msg.ranges[i] = scan_msg.range_max - 0.2;
-				else if(dist[540-i] == 0.0)
-					if (min_as_zero)
-						scan_msg.ranges[i] = 0.0;
-					else
-						scan_msg.ranges[i] = std::numeric_limits<float>::infinity();
-				else
-					scan_msg.ranges[i] = dist[540-i] / 1000.0;
-				scan_msg.intensities[i] = floor(intensities[540-i]);
-			}
-	}
-    for (int i = 0; i < 360; i++){
-        if((i >= angle_disable_min) && (i < angle_disable_max))		// disable angle part
-        {
+    else {
+        for (int i = 0; i <= 179; i++) {
+            if(dist[179-i] == 0.0 && zero_as_max)
+                scan_msg->ranges[i] = scan_msg->range_max - 0.2;
+            else if(dist[179-i] == 0.0)
+                if (min_as_zero)
+                    scan_msg->ranges[i] = 0.0;
+                else
+                    scan_msg->ranges[i] = std::numeric_limits<float>::infinity();
+            else
+                scan_msg->ranges[i] = dist[179-i] / 1000.0;
+            scan_msg->intensities[i] = floor(intensities[179-i]);
+        }
+        for (int i = 180; i < 360; i++) {
+            if(dist[540-i] == 0.0 && zero_as_max)
+                scan_msg->ranges[i] = scan_msg->range_max - 0.2;
+            else if(dist[540-i] == 0.0)
+                if (min_as_zero)
+                    scan_msg->ranges[i] = 0.0;
+                else
+                    scan_msg->ranges[i] = std::numeric_limits<float>::infinity();
+            else
+                scan_msg->ranges[i] = dist[540-i] / 1000.0;
+            scan_msg->intensities[i] = floor(intensities[540-i]);
+        }
+    }
+    for (int i = 0; i < 360; i++) {
+        if((i >= angle_disable_min) && (i < angle_disable_max)) {
             if (min_as_zero)
-                scan_msg.ranges[i] = 0.0;
+                scan_msg->ranges[i] = 0.0;
             else
-                scan_msg.ranges[i] = std::numeric_limits<float>::infinity();
+                scan_msg->ranges[i] = std::numeric_limits<float>::infinity();
         }
     }
 
-	pub->publish(scan_msg);
+    pub->publish(*scan_msg);
 }
 
-void startStopCB(const std_msgs::Int32ConstPtr msg)
+class LaserNode : public rclcpp::Node
 {
-	Command cmd = (Command) msg->data;
-	switch (cmd)
-	{
-	case STOP_DATA:
-		if (!is_scan_stop)
-		{
-			driver.StopScan(STOP_DATA);
-			is_scan_stop = true;
-			ROS_INFO("stop scan");
-		}
-		break;
-	case STOP_MOTOR:
-		if (!is_scan_stop)
-		{
-			driver.StopScan(STOP_DATA);
-			is_scan_stop = true;
-			ROS_INFO("stop scan");
-		}
-		if (!is_motor_stop)
-		{
-			driver.StopScan(STOP_MOTOR);
-			is_motor_stop = true;
-			ROS_INFO("stop motor");
-		}
-		break;
-	case START_MOTOR_AND_SCAN:
-		if (is_scan_stop)
-		{
-			ROS_INFO("start scan");
-			int res = driver.StartScan();
-			ROS_INFO("start: %d", res);
-			is_scan_stop = false;
-			is_motor_stop = false;
-		}
-		break;
-	default:
-		ROS_WARN("Unkonw command: %d ", cmd);
-		break;
-	}
-}
+public:
+    LaserNode() : Node("ls01g")
+    {
+        // Declare and get parameters
+        this->declare_parameter("scan_topic", "scan");
+        this->declare_parameter("laser_link", "laser_link");
+        this->declare_parameter("serial_port", "/dev/ttyUSB0");
+        this->declare_parameter("angle_disable_min", -1.0);
+        this->declare_parameter("angle_disable_max", -1.0);
+        this->declare_parameter("zero_as_max", true);
+        this->declare_parameter("min_as_zero", true);
+        this->declare_parameter("inverted", true);
 
-int main(int argv, char **argc)
+        scan_topic_ = this->get_parameter("scan_topic").as_string();
+        laser_link = this->get_parameter("laser_link").as_string();
+        string port = this->get_parameter("serial_port").as_string();
+        angle_disable_min = this->get_parameter("angle_disable_min").as_double();
+        angle_disable_max = this->get_parameter("angle_disable_max").as_double();
+        zero_as_max = this->get_parameter("zero_as_max").as_bool();
+        min_as_zero = this->get_parameter("min_as_zero").as_bool();
+        inverted = this->get_parameter("inverted").as_bool();
+
+        // Create publisher and subscriber
+        scan_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>(scan_topic_, 1000);
+        stop_sub_ = this->create_subscription<std_msgs::msg::Int32>(
+            "startOrStop", 10, 
+            std::bind(&LaserNode::start_stop_callback, this, std::placeholders::_1));
+
+        // Initialize serial
+        int ret = driver.OpenSerial(port.c_str(), B230400);
+        if (ret < 0) {
+            RCLCPP_ERROR(this->get_logger(), "Could not open port: %s", port.c_str());
+            return;
+        }
+        RCLCPP_INFO(this->get_logger(), "Open port: %s", port.c_str());
+
+        if (inverted) {
+            RCLCPP_INFO(this->get_logger(), "This laser is inverted, zero degree direction is align with line");
+        }
+
+        driver.StartScan();
+        RCLCPP_INFO(this->get_logger(), "Send start command successfully");
+
+        // Start processing loop
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(10),
+            std::bind(&LaserNode::timer_callback, this));
+    }
+
+    ~LaserNode() {
+        driver.StopScan(STOP_DATA);
+        driver.StopScan(STOP_MOTOR);
+        driver.CloseSerial();
+        RCLCPP_INFO(this->get_logger(), "Keyboard Interrupt, ls01g stop!");
+    }
+
+private:
+    void start_stop_callback(const std_msgs::msg::Int32::SharedPtr msg)
+    {
+        Command cmd = (Command)msg->data;
+        switch (cmd) {
+            case STOP_DATA:
+                if (!is_scan_stop) {
+                    driver.StopScan(STOP_DATA);
+                    is_scan_stop = true;
+                    RCLCPP_INFO(this->get_logger(), "stop scan");
+                }
+                break;
+            case STOP_MOTOR:
+                if (!is_scan_stop) {
+                    driver.StopScan(STOP_DATA);
+                    is_scan_stop = true;
+                    RCLCPP_INFO(this->get_logger(), "stop scan");
+                }
+                if (!is_motor_stop) {
+                    driver.StopScan(STOP_MOTOR);
+                    is_motor_stop = true;
+                    RCLCPP_INFO(this->get_logger(), "stop motor");
+                }
+                break;
+            case START_MOTOR_AND_SCAN:
+                if (is_scan_stop) {
+                    RCLCPP_INFO(this->get_logger(), "start scan");
+                    int res = driver.StartScan();
+                    RCLCPP_INFO(this->get_logger(), "start: %d", res);
+                    is_scan_stop = false;
+                    is_motor_stop = false;
+                }
+                break;
+            default:
+                RCLCPP_WARN(this->get_logger(), "Unknown command: %d", cmd);
+                break;
+        }
+    }
+
+    void timer_callback()
+    {
+        if (is_scan_stop)
+            return;
+
+        double angle[PACKLEN + 10];
+        double distance[PACKLEN + 10];
+        double data[PACKLEN + 10];
+        double data_intensity[PACKLEN + 10];
+        double speed;
+        
+        memset(data, 0, sizeof(data));
+        int ret = driver.GetScanData(angle, distance, PACKLEN, &speed);
+        for (int i = 0; i < ret; i++) {
+            data[i] = distance[i];
+            data_intensity[i] = angle[i];
+        }
+        
+        static rclcpp::Time starts = this->now();
+        rclcpp::Time ends = this->now();
+        float scan_duration = (ends - starts).seconds() * 1e-3;
+        
+        publish_scan(scan_pub_, data, data_intensity, ret, starts, scan_duration);
+        starts = ends;
+        
+        RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 30000, "ls01g works fine!");
+    }
+
+    rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr scan_pub_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr stop_sub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    string scan_topic_;
+};
+
+int main(int argc, char *argv[])
 {
-	ros::init(argv, argc, "ls01g");
-	ros::NodeHandle n;
-	string scan_topic = "scan";
-	string port = "/dev/ttyUSB0";
-	ros::param::get("~scan_topic", scan_topic);
-	ros::param::get("~laser_link", laser_link);
-	ros::param::get("~serial_port", port);
-	ros::param::get("~angle_disable_min", angle_disable_min);
-	ros::param::get("~angle_disable_max", angle_disable_max);
-	ros::param::get("~zero_as_max", zero_as_max);
-	ros::param::get("~min_as_zero", min_as_zero);
-	ros::param::get("~inverted", inverted);
-	ros::Publisher scan_pub = n.advertise<sensor_msgs::LaserScan>(scan_topic, 1000);
-	ros::Subscriber stop_sub = n.subscribe<std_msgs::Int32>("startOrStop", 10, startStopCB);
-	int ret;
-	ret = driver.OpenSerial(port.c_str(), B230400);
-	if (ret < 0)
-	{
-		ROS_ERROR("could not open port:%s", port.c_str());
-		return 0;
-	}
-	else
-	{
-		ROS_INFO("open port:%s", port.c_str());
-	}
-
-	if (inverted)
-	{
-		ROS_INFO("This laser is inverted, zero degree direction is align with line");
-	}
-
-	driver.StartScan();
-
-	ROS_INFO("Send start command successfully");
-
-	double angle[PACKLEN + 10];
-	double distance[PACKLEN + 10];
-	double data[PACKLEN + 10];
-	double data_intensity[PACKLEN + 10];
-	double speed;
-	int count = 0;
-
-	ros::Time starts = ros::Time::now();
-	ros::Time ends = ros::Time::now();
-	ROS_INFO("talker....");
-	while (ros::ok())
-	{
-		ros::spinOnce();
-
-		if (is_scan_stop)
-			continue;
-
-		// ROS_INFO_THROTTLE(2, "talker");
-		memset(data, 0, sizeof(data));
-		int ret = driver.GetScanData(angle, distance, PACKLEN, &speed);
-		for (int i = 0; i < ret; i++)
-		{
-			data[i] = distance[i];
-			data_intensity[i] = angle[i];
-		}
-		ROS_INFO_THROTTLE(30, "ls01g works fine!");
-		ends = ros::Time::now();
-		float scan_duration = (ends - starts).toSec() * 1e-3;
-		publish_scan(&scan_pub, data, data_intensity, ret, starts, scan_duration);
-		starts = ends;
-	}
-	driver.StopScan(STOP_DATA);
-	driver.StopScan(STOP_MOTOR);
-	driver.CloseSerial();
-	ROS_INFO("Keyboard Interrupt, ls01g stop!");
-
-	return 0;
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<LaserNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
-
